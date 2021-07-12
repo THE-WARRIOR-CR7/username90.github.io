ad39f608b5ae45f8af3b7ce670bbdf3091920578
THE-WARRIOR-CR7 <82370377+THE-WARRIOR-CR7@users.noreply.github.com>
2021-07-12 09:49:01 -0700

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,7 +1,7 @@
 function adduser() {
     player1Name=document.getElementById("player1_input").value;
     player2Name=document.getElementById("player2_input").value;
-    localStorage.setItem("player1name",player1Name);
-    localStorage.setItem("player2name",player2Name);
+    localStorage.setItem("player1_name",player1Name);
+    localStorage.setItem("player2_name",player2Name);
     window.location="game_page.html";
 }
